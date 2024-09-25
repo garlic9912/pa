9505096d025bc4055abc77d978e7762e74aa7c70
tracer-ics2023 <tracer@njuics.org>
2024-09-25 21:53:55 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:53:55 up 11:45,  1 user,  load average: 1.00, 0.76, 0.64

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,22 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  if (s == NULL) return 0;
+  int i = 0, len = 0;
+  while (s[i] != '\0') {
+    len++;
+    i++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  int len = strlen(src);
+  int i = 0, j = 0;
+  while (j <= len) {
+    dst[i++] = src[j++];
+  }
+  return dst;  
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +28,30 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int len1 = strlen(dst), len2 = strlen(src);
+  if (len2 == 0) return dst;
+  int i = len1, j = 0;
+  while (j <= len2) {
+    dst[i++] = src[j++];
+  }
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i = 0, j = 0;
+  char c1, c2;
+  int len1 = strlen(s1);
+  int len2 = strlen(s2);
+  while (i <= len1 && j <= len2) {
+    c1 = s1[i++];  
+    c2 = s2[j++];
+    if (c1 == c2) continue;
+    else if (c1 < c2) return -1;
+    else return 1;
+  }
+  if (len1 < len2) return -1;
+  else if (len1 > len2) return 1;
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +59,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char a = (char)c;
+  char *byte = (char *)s;
+  for (int i = 0; i < n; i++) {
+    byte[i] = a;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +76,16 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if (sizeof(s1) < n || sizeof(s2) < n) {
+    assert(0);
+  }
+  char *byte_s1 = (char *)s1;
+  char *byte_s2 = (char *)s2;
+  for (int i = 0; i < n; i++) {
+    if (byte_s1[i] < byte_s2[i]) return -1;
+    else if (byte_s1[i] > byte_s2[i]) return 1;
+  }
+  return 0;
 }
 
 #endif
