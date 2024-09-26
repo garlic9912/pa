6206b86330c9048ee8b7d35af18664fd7001db21
tracer-ics2023 <tracer@njuics.org>
2024-09-26 22:14:30 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:14:30 up 11:49,  1 user,  load average: 1.13, 1.18, 0.91

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -14,8 +14,9 @@ size_t strlen(const char *s) {
   return len;
 }
 
+
 char *strcpy(char *dst, const char *src) {
-  int len = strlen(src);
+  size_t len = strlen(src);
   int i = 0, j = 0;
   while (j <= len) {
     dst[i++] = src[j++];
@@ -28,7 +29,8 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  int len1 = strlen(dst), len2 = strlen(src);
+  size_t len1 = strlen(dst);
+  size_t len2 = strlen(src);
   if (len2 == 0) return dst;
   int i = len1, j = 0;
   while (j <= len2) {
@@ -40,8 +42,8 @@ char *strcat(char *dst, const char *src) {
 int strcmp(const char *s1, const char *s2) {
   int i = 0, j = 0;
   char c1, c2;
-  int len1 = strlen(s1);
-  int len2 = strlen(s2);
+  size_t len1 = strlen(s1);
+  size_t len2 = strlen(s2);
   while (i <= len1 && j <= len2) {
     c1 = s1[i++];  
     c2 = s2[j++];
@@ -59,13 +61,15 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  char a = (char)c;
+  // char * default
   char *byte = (char *)s;
   for (int i = 0; i < n; i++) {
-    byte[i] = a;
+    byte[i] = c;
   }
+  byte[n] = '\0';
   return s;
 }
+
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
@@ -76,16 +80,13 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  if (sizeof(s1) < n || sizeof(s2) < n) {
-    assert(0);
-  }
-  char *byte_s1 = (char *)s1;
-  char *byte_s2 = (char *)s2;
-  for (int i = 0; i < n; i++) {
-    if (byte_s1[i] < byte_s2[i]) return -1;
-    else if (byte_s1[i] > byte_s2[i]) return 1;
-  }
-  return 0;
+    int reval;
+    while(n && !(reval = (*(unsigned char *)s1) - (*(unsigned char *)s2)))
+    {
+        s1 = (unsigned char *)s1 + 1;
+        s2 = (unsigned char *)s2 + 1;
+        --n;
+    }
+    return reval;
 }
-
 #endif
