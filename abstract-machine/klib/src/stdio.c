7e6e1ee9bdcc6ca9d2ad5c57c8fa83af5d80fde5
tracer-ics2023 <tracer@njuics.org>
2024-09-28 10:55:39 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:55:39 up 18:32,  1 user,  load average: 1.77, 1.04, 0.78

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  // i -> fmt
+  // count -> out
+  int i = 0, count = 0;
+  while (fmt[i] != '\0') {
+    char c;
+    if (fmt[i] == '%') {
+      c = fmt[++i];
+      switch(c) {
+        case 's': 
+          char *str = va_arg(args, char *);
+          int len1 = strlen(str);
+          strcpy(out+count, str);
+          count += len1;
+          break;
+        case 'd':
+          int idx = 0;
+          char buf[32];
+          int num = va_arg(args, int);
+          while (num != 0) {
+            buf[idx++] = '0' + (num % 10);
+            num /= 10;
+          }
+          buf[idx] = '\0';
+          // reversal
+          int len2 = strlen(buf);
+          for (int k = 0; k < len2 / 2; k++) {
+              char temp = buf[k];
+              buf[k] = buf[len2 - k - 1];
+              buf[len2 - k - 1] = temp;
+          }
+          strcpy(out+count, buf);
+          count += len2;
+          break;
+      } 
+      i++;
+    } else {
+      out[count++] = fmt[i++];
+    }
+  }
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
