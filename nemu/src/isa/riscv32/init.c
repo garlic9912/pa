7b81c1bd9bc59f5c84cb56bfbdf950ce66284c18
tracer-ics2023 <tracer@njuics.org>
2024-09-29 14:53:26 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:53:26 up  2:59,  1 user,  load average: 0.48, 0.42, 0.28

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -26,12 +26,12 @@ static const uint32_t img [] = {
   0x00000a13,   // li	s4,0
   0x00000a13,   // li	s4,0
   0x00000a13,   // li	s4,0
-  0x00000a13,   // li	s4,0
-  0x00000a13,   // li	s4,0
-  0x00000a13,   // li	s4,0
-  0x00000a13,   // li	s4,0
   0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu  a0, -0xf0(t0)   
+  0x0102c503,  // lbu  a0, 0x80000010
+  0x0102c503,  // lbu  a0, 0x80000010 
+  0x0102c503,  // lbu  a0, 0x80000010 
+  0x0102c503,  // lbu  a0, 0x80000010 
+  0xf102c503,  // lbu  a0, 0x80000010     
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
