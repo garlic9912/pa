c8d7a324b4e637708bfc6d1dbd4793f23f33fd8c
tracer-ics2023 <tracer@njuics.org>
2024-09-30 15:38:27 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:38:27 up  5:26,  1 user,  load average: 0.52, 0.41, 0.50

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -108,8 +108,6 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
-  printf("elf_file:%s\n", elf_file);
-  panic("1111111111111111111111111111");
 
   /* Set random seed. */
   init_rand();
