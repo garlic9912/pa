fe5f8fec15328e364c0a6e4cf54cca821841846d
tracer-ics2023 <tracer@njuics.org>
2024-10-02 15:42:54 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:42:54 up  4:10,  1 user,  load average: 1.12, 1.03, 0.86

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -225,9 +225,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  if (g_print_step) { 
-    IFDEF(CONFIG_ITRACE, puts(_this->logbuf));  
-  }
+  // if (g_print_step) { 
+  //   IFDEF(CONFIG_ITRACE, puts(_this->logbuf));  
+  // }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 // watch point function
@@ -246,8 +246,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 
 #ifdef CONFIG_FTRACE
-  if(flag-- > 0) init_elf_file();
-  // ftrace(pc, cpu.pc);
+  init_elf_file();
+  ftrace(pc, cpu.pc);
   free(buf);
 #endif
 
