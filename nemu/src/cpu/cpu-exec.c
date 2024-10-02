f3b9248db5993115368cbf9f6b7e5402bdca5fbe
tracer-ics2023 <tracer@njuics.org>
2024-10-02 15:28:26 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:26 up  3:55,  1 user,  load average: 1.09, 0.84, 0.69

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,6 +55,9 @@ static int flag = 0;
 #define ELFCLASS32 1
 #define ELFCLASS64 2
 
+#define ELF32_ST_TYPE(info) (((info) >> 4) & 0xf)
+#define STT_FUNC    2
+
 // ELF header structure definitions
 typedef struct {
     unsigned char e_ident[EI_NIDENT]; // ELF file identification
@@ -174,6 +177,7 @@ void ftrace(word_t old_pc, word_t new_pc) {
   if (new_pc == old_pc + 4) return;
   // acquire the name of pc_position
   for (int i = 0; i < shdr[symtab_idx].sh_size / sizeof(Elf32_Sym); ++i) {
+    if (ELF32_ST_TYPE(symtab[i].st_info) != STT_FUNC) continue;
     char *name = strtab + symtab[i].st_name;
     if (name == NULL) continue;
     word_t pos_start = symtab[i].st_value;
@@ -222,9 +226,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  // if (g_print_step) { 
-  //   IFDEF(CONFIG_ITRACE, puts(_this->logbuf));  
-  // }
+  if (g_print_step) { 
+    IFDEF(CONFIG_ITRACE, puts(_this->logbuf));  
+  }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 // watch point function
