b20b5a7b64de3a460728035b75a1e4c8faa9089b
tracer-ics2023 <tracer@njuics.org>
2024-10-02 14:57:30 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:57:30 up  3:24,  1 user,  load average: 0.77, 0.57, 0.53

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -160,9 +160,7 @@ void init_elf_file() {
   // }
 
   // Free allocated memory and close the file
-  free(buf);
   fclose(fp);  
-
   for (int i = 0; i < shdr[symtab_idx].sh_size / sizeof(Elf32_Sym); ++i) {
     const char *name = strtab + symtab[i].st_name;
     printf("Symbol: %s, Value: 0x%08X, Size: %u\n", name, symtab[i].st_value, symtab[i].st_size);
