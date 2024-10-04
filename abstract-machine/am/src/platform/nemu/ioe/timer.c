d622e12a00369a7cf00ace30f76383acf858180d
tracer-ics2023 <tracer@njuics.org>
2024-10-04 15:23:12 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:23:12 up 1 day,  2:51,  1 user,  load average: 1.11, 1.15, 0.98

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,12 +5,12 @@
 static uint64_t sys_init_time;
 
 void __am_timer_init() {
-  sys_init_time =(  ( (uint64_t)inl(RTC_ADDR+4) << 32 ) + (uint64_t)inl(RTC_ADDR) );  
+  ioe_read(6, &sys_init_time);  
 }
 
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (  ( (uint64_t)inl(RTC_ADDR + 4) << 32 ) | (uint64_t)inl(RTC_ADDR)  );
+  ioe_read(6, uptime);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
