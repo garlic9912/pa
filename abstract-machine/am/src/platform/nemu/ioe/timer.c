44bd84ef5d836ea7ad6234ff2ad2d2bc430283c4
tracer-ics2023 <tracer@njuics.org>
2024-10-04 15:06:05 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:06:05 up 1 day,  2:34,  1 user,  load average: 0.60, 0.64, 0.63

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,14 @@
 #include <am.h>
 #include <nemu.h>
-#include <sys/time.h>
+#include <time.h>
 
-// static struct timeval boot_time = {};
 
 void __am_timer_init() {
 }
 
+
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // struct timeval now;
-  // gettimeofday(&now, NULL);
-  // long seconds = now.tv_sec - boot_time.tv_sec;
-  // long useconds = now.tv_usec - boot_time.tv_usec;
-  // uptime->us = seconds * 1000000 + (useconds + 500);
+  ioe_read(6, uptime);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
