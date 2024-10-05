3b47e5d975bc97d0287cfaa307f10c21af26ddc5
tracer-ics2023 <tracer@njuics.org>
2024-10-05 20:10:03 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:10:03 up 5 min,  1 user,  load average: 0.82, 0.50, 0.23

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,7 +58,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_DTRACE
   printf("device read: %s => 0x%x\n", map->name, addr);
+  #endif
   return ret;
 }
 
@@ -67,6 +69,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+  #ifdef CONFIG_DTRACE
   printf("device write: %s => 0x%x\n", map->name, addr);
+  #endif
   invoke_callback(map->callback, offset, len, true);
 }
