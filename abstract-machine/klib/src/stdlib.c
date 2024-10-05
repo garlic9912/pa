ca6c8341812e7771d197f4aa974a28db759a19d8
tracer-ics2023 <tracer@njuics.org>
2024-10-05 15:35:44 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:35:44 up 35 min,  1 user,  load average: 0.36, 0.56, 0.60

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,6 +5,9 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
+static char *addr;
+static int init = 1;
+
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -34,12 +37,17 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if (init-- > 0) addr = (void *)ROUNDUP(heap.start, 8);
+  size  = (size_t)ROUNDUP(size, 8);
+  char *old = addr;
+  addr += size;
+  return old;
 #endif
   return NULL;
 }
 
 void free(void *ptr) {
+  
 }
 
 #endif
