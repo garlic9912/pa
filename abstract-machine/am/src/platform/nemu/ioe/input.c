9e79a4fbb64b7c43d36bb8f32847d7a1b30241cd
tracer-ics2023 <tracer@njuics.org>
2024-10-06 21:03:07 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:03:07 up  8:24,  1 user,  load average: 0.62, 0.69, 0.64

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,18 +5,17 @@
 #define KEYDOWN_MASK 0x8000
 
 
-static uint32_t keymap[83] = {
-  AM_KEY_NONE,
-  AM_KEYS(AM_KEY_NAMES)
-};
-
-
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = false;
-  kbd->keycode = AM_KEY_NONE;
-  uint8_t code = inb(KBD_ADDR);
-  if (keymap[code] != AM_KEY_NONE) {
-    kbd->keycode = keymap[code];
-    kbd->keydown = true;
+  uint32_t am_scancode = inl(KBD_ADDR);
+  // 最高位是判断是按下还是弹回
+  if (am_scancode & KEYDOWN_MASK) // 最高位是 1 按下
+  {
+    kbd->keydown = 1;
+    kbd->keycode = am_scancode ^ KEYDOWN_MASK;
+  }
+  else
+  {
+    kbd->keydown = 0;
+    kbd->keycode = am_scancode;
   }
 }
