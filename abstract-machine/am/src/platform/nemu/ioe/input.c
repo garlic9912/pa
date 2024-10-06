942f7f0599636dc059c018a14cdda6e706e45f85
tracer-ics2023 <tracer@njuics.org>
2024-10-06 19:52:07 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:52:07 up  7:13,  1 user,  load average: 0.40, 0.55, 0.61

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,14 +5,19 @@
 #define KEYDOWN_MASK 0x8000
 
 // static uint32_t keymap[256] = {};
+// #define SDL_KEYMAP(k) keymap[AM_KEY_ ## k] = AM_KEY_ ## k;
+
+// static void init_keymap() {
+//   AM_KEYS(SDL_KEYMAP);
+// }
 
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = false;
   kbd->keycode = AM_KEY_NONE;
 
-  uint8_t code = inb(KBD_ADDR) | KEYDOWN_MASK;
-  if (code != AM_KEY_NONE) {
+  uint8_t code = inb(KBD_ADDR);
+  if (code != 0) {
     kbd->keycode = code;
     kbd->keydown = true;
   }
