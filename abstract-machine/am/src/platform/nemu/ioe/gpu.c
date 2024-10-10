3f963aafc5e04e3b5322b6b05c9f83dc8123a487
tracer-ics2023 <tracer@njuics.org>
2024-10-10 17:15:06 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:15:06 up  5:34,  1 user,  load average: 0.69, 0.59, 0.64

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,9 +24,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int width = inl(VGACTL_ADDR) >> 16;
   int w = ctl->w, h = ctl->h;
+  int N = width / w;
   int block_size = w * h;
-  int x = ctl->x, y = ctl->y;
-  uint32_t start = FB_ADDR + x + y * width;
+  int x = ctl->x / w, y = ctl->y / h;
+  uint32_t start = FB_ADDR + x*block_size*4 + y * N * block_size * 4;
   for (int i = 0; i < block_size; i++) {
     outl(start + i*4, *((uint32_t *)(ctl->pixels) + i));
   }
