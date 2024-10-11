113d18e89d6650e6dff33e7a09ca1707a6e38b6c
tracer-ics2023 <tracer@njuics.org>
2024-10-11 15:47:22 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:47:22 up  4:11,  1 user,  load average: 0.91, 0.71, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,19 +22,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int width = inl(VGACTL_ADDR) >> 16;
-  int w = ctl->w, h = ctl->h;
-  // int N = (int)(width / w);
-  int block_size = w * h;
-  int x = ctl->x, y = ctl->y;
-  uint32_t start = FB_ADDR + x*h*4 + y*width*4;
-  for (int i = 0; i < block_size; i++) {
-    outl(start + i*4, *((uint32_t *)(ctl->pixels) + i));
-  }
-  // sync
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }  
+  int i;
+  // 获取宽度（从vga.c的初始化文件中可以得知）
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
+  // 获取显存起始地址
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // 向显存写入内容
+  for (i = 0; i < w * h; i++) fb[i] = 0xFF000000; // 显存初始化，初始背景
+  // 向同步寄存器写入1，让其更新屏幕
+  outl(SYNC_ADDR, 1); 
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
