612b9a8364f5d959c880d76552d1c0a75f7b0a47
tracer-ics2023 <tracer@njuics.org>
2024-10-13 16:04:41 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:04:41 up  6:16,  1 user,  load average: 2.03, 1.07, 1.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -22,19 +23,25 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int width = inl(VGACTL_ADDR) >> 16;
-  int w = ctl->w, h = ctl->h;
-  // int N = (int)(width / w);
-  int block_size = w * h;
-  int x = ctl->x, y = ctl->y;
-  uint32_t start = FB_ADDR + x*h*4 + y*width*4;
-  for (int i = 0; i < block_size; i++) {
-    outl(start + i*4, *((uint32_t *)(ctl->pixels) + i));
+  // 向从坐标(ctl->x, ctl->y)开始，绘制ctl->w * ctl->h的矩形图像. 图像像素按行优先方式存储在ctl->pixels中
+  // 若ctl->sync为true, 则马上将帧缓冲中的内容同步到屏幕上，绘制到屏幕的方法就是将像素ctl->pixels拷贝到显存FB_ADDR指定位置
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  int width = io_read(AM_GPU_CONFIG).width;
+  // 获取像素
+  uint32_t *pixels = ctl->pixels;
+  // 获取显存
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // 防止越界！！！
+  uint32_t len = sizeof(uint32_t) * (w < width - x ? w : width - x);
+  int pos_pixels = 0;
+  for(int j = y; j < y + h; j++, pos_pixels += w){
+    // 二维数组类似fb + j * width + x，整行拷贝更节省时间
+    memcpy(fb + j * width + x, pixels + (pos_pixels), len);
   }
-  // sync
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
-  }  
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
