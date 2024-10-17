4f3f95f02a55ef3d0859045ec1d0116857441c70
tracer-ics2023 <tracer@njuics.org>
2024-10-17 22:08:38 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:08:38 up 11:51,  1 user,  load average: 0.60, 0.54, 0.66

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -23,6 +23,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr[0x341] = epc;
   // mcause <- NO
   cpu.csr[0x342] = NO;
+  printf("%x\n", NO);
   // 处理程序的地址 mtvec
   word_t addr = cpu.csr[0x305];
   return addr;
