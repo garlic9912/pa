094370b42101c555376f4a86f88f4e43c7696284
tracer-ics2023 <tracer@njuics.org>
2024-10-17 22:22:17 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:22:17 up 12:05,  1 user,  load average: 1.03, 0.70, 0.69

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,10 +8,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case (uint32_t)(-1): ev.event = EVENT_YIELD; break;
-      default: 
-      printf("%d\n", (int)(c->mcause));
-      ev.event = EVENT_ERROR; break;
+      case -1: ev.event = EVENT_YIELD; break;
+      default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
     assert(c != NULL);
