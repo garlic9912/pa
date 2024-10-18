19106075465a1ec90cf6f9ad5f50fb5fb5cad1cb
tracer-ics2023 <tracer@njuics.org>
2024-10-18 16:54:00 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:54:00 up  7:17,  1 user,  load average: 0.44, 0.47, 0.55

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,7 +20,6 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != cpu.gpr[i]) {
-      printf("0x%x\n", pc);
       return false;
     }
   }
