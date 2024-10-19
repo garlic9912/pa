e8f647ebb60691bc5a4ebb3f74fa069010ceb507
tracer-ics2023 <tracer@njuics.org>
2024-10-19 15:57:41 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:57:41 up  6:12,  1 user,  load average: 0.77, 1.06, 0.89

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -65,9 +65,9 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, s->dnpc = CSRs(0x341));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, s->dnpc = CSRs(0x341) + 4);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrr   , I, word_t t = CSRs(imm); CSRs(imm) = t | src1; R(rd) = t);
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(R(17) , s->snpc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(R(17) , s->snpc-4));
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrw   , I, word_t t = CSRs(imm); CSRs(imm) = src1; R(rd) = t);
 
   INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu  , R, uint64_t result = (uint64_t) src1 * (uint64_t) src2; R(rd) = result >> 32;);
