37e29f379ec566a23e190e8b035370414dd39683
tracer-ics2023 <tracer@njuics.org>
2024-10-22 21:58:44 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:58:44 up 11:16,  1 user,  load average: 0.96, 0.67, 0.71

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = 400;  // TODO: get the correct width
-  // int h = 300;  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);  
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);  
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -22,15 +22,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int width = inl(VGACTL_ADDR) >> 16;
-  int w = ctl->w, h = ctl->h;
-  int x = ctl->x, y = ctl->y;
-  for (int i = y; i < y+h; i++) {
-    for (int j = x; j < x+w; j++) {
-      outl(FB_ADDR+4*i*width+4*j, *((uint32_t *)(ctl->pixels) + (i-y)*w + (j-x)));
-    }
-  }
-  // sync
+  // int width = inl(VGACTL_ADDR) >> 16;
+  // int w = ctl->w, h = ctl->h;
+  // int x = ctl->x, y = ctl->y;
+  // for (int i = y; i < y+h; i++) {
+  //   for (int j = x; j < x+w; j++) {
+  //     outl(FB_ADDR+4*i*width+4*j, *((uint32_t *)(ctl->pixels) + (i-y)*w + (j-x)));
+  //   }
+  // }
+  // // sync
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }  
