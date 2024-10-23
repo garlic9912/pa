e27821e1ff388199f7c00b9b66bdf81042734003
tracer-ics2023 <tracer@njuics.org>
2024-10-23 21:24:08 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:24:08 up 13:13,  1 user,  load average: 1.27, 1.09, 0.83

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,8 +19,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // Elf64_Ehdr 是 64 位系统的 ELF 头部结构体
   Elf64_Ehdr ehdr; 
   // 读取文件
-  panic("okokokokokokokokookokokok");
   ramdisk_read(&ehdr, 0, get_ramdisk_size());
+  panic("okokokokokokokokookokokok");
   // 读取 Program Headers
   Elf64_Phdr phdr[ehdr.e_phnum];
 for (int i = 0; i < ehdr.e_phnum; ++i) {
