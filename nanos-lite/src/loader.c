fba5cac2f781bb87aca5180e12fcc3f6347c3fdf
tracer-ics2023 <tracer@njuics.org>
2024-10-23 20:41:58 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:41:58 up 12:31,  1 user,  load average: 0.34, 0.59, 0.73

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,24 +16,25 @@ extern size_t get_ramdisk_size();
 // extern char *elf_loader_file;
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // Elf64_Ehdr 是 64 位系统的 ELF 头部结构体
-  Elf64_Ehdr ehdr; 
-  // 读取文件
-  ramdisk_read(&ehdr, 0, get_ramdisk_size());
-  // 读取 Program Headers
-  Elf64_Phdr phdr[ehdr.e_phnum];
-for (int i = 0; i < ehdr.e_phnum; ++i) {
-    printf("Program %d:\n", i);
-    printf("Type: %x\n", phdr[i].p_type);
-    printf("Offset: %lx\n", phdr[i].p_offset);
-    printf("Virtual Address: %lx\n", phdr[i].p_vaddr);
-    printf("Physical Address: %lx\n", phdr[i].p_paddr);
-    printf("File Size: %lx\n", phdr[i].p_filesz);
-    printf("Memory Size: %lx\n", phdr[i].p_memsz);
-    printf("Flags: %lx\n", phdr[i].p_flags);
-    printf("Alignment: %lx\n", phdr[i].p_align);
-}  
-panic("okokokokokokokokookokokok");
+  printf("%x\n", get_ramdisk_size());
+//   // Elf64_Ehdr 是 64 位系统的 ELF 头部结构体
+//   Elf64_Ehdr ehdr; 
+//   // 读取文件
+//   ramdisk_read(&ehdr, 0, get_ramdisk_size());
+//   // 读取 Program Headers
+//   Elf64_Phdr phdr[ehdr.e_phnum];
+// for (int i = 0; i < ehdr.e_phnum; ++i) {
+//     printf("Program %d:\n", i);
+//     printf("Type: %x\n", phdr[i].p_type);
+//     printf("Offset: %lx\n", phdr[i].p_offset);
+//     printf("Virtual Address: %lx\n", phdr[i].p_vaddr);
+//     printf("Physical Address: %lx\n", phdr[i].p_paddr);
+//     printf("File Size: %lx\n", phdr[i].p_filesz);
+//     printf("Memory Size: %lx\n", phdr[i].p_memsz);
+//     printf("Flags: %lx\n", phdr[i].p_flags);
+//     printf("Alignment: %lx\n", phdr[i].p_align);
+// }  
+// panic("okokokokokokokokookokokok");
   return 0;
 }
 
