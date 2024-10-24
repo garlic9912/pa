4f9e6e86e75751dbd87c4fcab94e466dff1a50e0
tracer-ics2023 <tracer@njuics.org>
2024-10-24 16:17:14 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:17:14 up  6:52,  1 user,  load average: 1.30, 0.80, 0.71

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      printf("hahahahaha\n"); break;
+      printf("yield\nyield\nyield\n"); break;
     case EVENT_SYSCALL:
       do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
