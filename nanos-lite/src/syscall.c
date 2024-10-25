fe61cb75a129e856535d7a26a23f1e0e6f5f6e37
tracer-ics2023 <tracer@njuics.org>
2024-10-25 16:26:25 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:26:25 up  9:04,  1 user,  load average: 0.84, 0.88, 0.78

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -59,7 +59,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_write:
       ret = sys_write(a[1], (char *)a[2], a[3]);
-      c->GPR1 = ret;
+      a[0] = ret;
       break;
     case SYS_yield: 
       ret = sys_yield();
