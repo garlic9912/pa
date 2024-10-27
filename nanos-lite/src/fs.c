b3bb73ce9bd0edb318fc07b9f862c32cb666118f
tracer-ics2023 <tracer@njuics.org>
2024-10-27 16:28:53 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:28:53 up  4:36,  1 user,  load average: 0.76, 0.77, 1.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -59,8 +59,8 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   size_t fsize, disk_offset, open_offset;
   fsize = file_table[fd].size;
+  panic("%d", fsize);
   disk_offset = file_table[fd].disk_offset;
-  panic("%d", disk_offset);
   open_offset = file_table[fd].open_offset;
   // 计算文件的读写指针偏移量
   size_t start_offset = disk_offset + open_offset;
