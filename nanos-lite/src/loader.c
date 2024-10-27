ae5f894337d1ffa376e94cce5c9524a150785099
tracer-ics2023 <tracer@njuics.org>
2024-10-27 21:52:59 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:52:59 up 10:00,  1 user,  load average: 1.05, 0.87, 0.61

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,6 +40,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // load the program
   for (int i = 0; i < ehdr.e_phnum; ++i) {
     if (phdr[i].p_type == PT_LOAD) {
+      printf("%x, %x, %x, %x\n", phdr[i].p_offset, phdr[i].p_vaddr, phdr[i].p_filesz, phdr[i].p_memsz);
       fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
       fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_memsz);
       memset((void *)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
