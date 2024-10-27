88d06c52218608c85a326c432aaa7fc820a84f8f
tracer-ics2023 <tracer@njuics.org>
2024-10-27 20:36:44 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:36:44 up  8:44,  1 user,  load average: 0.77, 0.64, 0.79

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -129,10 +129,10 @@ size_t fs_lseek(int fd, size_t offset, int whence){
 
 
 int fs_close(int fd) {
-  if (fd <= 2 || fd > sizeof(file_table)/sizeof(Finfo)) {
-    panic("fd:%d is wrong", fd);
-  }
-  file_table[fd].open_offset = 0;
+  // if (fd <= 2 || fd > sizeof(file_table)/sizeof(Finfo)) {
+  //   panic("fd:%d is wrong", fd);
+  // }
+  // file_table[fd].open_offset = 0;
   return 0;
 }
 
