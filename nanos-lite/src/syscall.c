f056472b3140324e38d948a5807a085af48e7d0d
tracer-ics2023 <tracer@njuics.org>
2024-10-28 21:41:33 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:41:33 up  9:32,  1 user,  load average: 0.72, 0.93, 0.88

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,8 +42,8 @@ struct timeval
 
 int sys_gettimeofday(struct timeval *tv) {
   // 获取当前时间
-  panic("11111111111111111111111");
   tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
+  panic("%d", tv->tv_sec);
   tv->tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
   return 1;
 }
