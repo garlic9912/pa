e457a5a8f11a13374e8f0644838d908ef2d19500
tracer-ics2023 <tracer@njuics.org>
2024-10-28 14:48:12 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:48:12 up  2:39,  1 user,  load average: 0.55, 0.74, 0.71

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,6 +17,7 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   for (int i = 0; i < len; i++) {
+    panic("1111111111111111");
     putch(*((char *)buf + i));
   }
   return len;
