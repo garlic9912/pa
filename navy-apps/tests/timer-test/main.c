e41a08d312666999521fecb79c14b8e0d31a5d79
tracer-ics2023 <tracer@njuics.org>
2024-10-28 22:26:50 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:26:50 up 10:17,  1 user,  load average: 0.92, 0.83, 0.76

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,14 +4,16 @@
 
 int main() {
     struct timeval time;
-    int stime = 1;
-    while (1) {
-        while (time.tv_sec * 2 < stime) {
+    int stime = 0;
+    int utime = 500000;
+    int tol_time = stime * 1000000 + utime;
+    while (1) { 
+        while ((int)time.tv_sec * 1000000 + (int)time.tv_usec < tol_time) {
             gettimeofday(&time, NULL);
             // printf("%d\n", (int)time.tv_usec);
         } 
-        printf("time: %d\n", (int)time.tv_usec);
-        stime += 1;
+        printf("time: %d\n", (int)stime);
+        tol_time += 500000;
     }
     return 0;
 }
