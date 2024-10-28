b08e898cb107c4254ba0a502b93be586a93930a1
tracer-ics2023 <tracer@njuics.org>
2024-10-28 14:52:02 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:52:02 up  2:43,  1 user,  load average: 1.26, 0.85, 0.75

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,9 +84,10 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   if (fd == FD_STDOUT || fd == FD_STDERR) {
     // 输出到串口, 即设置对应的写函数
       for (int i = 0; i < len; i++) {
-    putch(*((char *)buf + i));
-    return len;
-  }
+        putch(*((char *)buf + i));
+      
+      }
+      return len;
   }
 
   size_t fsize, disk_offset, open_offset;
