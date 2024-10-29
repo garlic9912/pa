f78c96cbaed5638aedce36853f0b75c5c7398b9c
tracer-ics2023 <tracer@njuics.org>
2024-10-29 14:14:19 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:14:19 up  7:24,  1 user,  load average: 1.73, 1.30, 1.20

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,14 +2,11 @@
 #include <assert.h>
 #include <sys/time.h>
 
+extern int NDL_GetTicks();
 int main() {
-    struct timeval time;
     int tol_time = 500000;
     while (1) { 
-        while (NDL_GetTicks() < tol_time) {
-            gettimeofday(&time, NULL);
-            // printf("%d\n", (int)time.tv_usec);
-        } 
+        while (NDL_GetTicks() < tol_time);
         printf("time: %d\n", (int)tol_time / 500000);
         tol_time += 500000;
     }
