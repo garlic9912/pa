6262901f5b2d8e26adb924702c94b22a0c18b587
tracer-ics2023 <tracer@njuics.org>
2024-10-29 16:41:33 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:41:33 up  9:51,  1 user,  load average: 0.87, 0.60, 0.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,7 +27,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == 0) return 0;
-  panic("1111111111111111");
+  // panic("1111111111111111");
   // 假设一次只读出一个事件
   char ev_info[30];
   sprintf(ev_info, "Got  (kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
