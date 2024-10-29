60dc75c7024fcb908d42077e77020249998a25e3
tracer-ics2023 <tracer@njuics.org>
2024-10-29 17:16:34 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:16:34 up 10:26,  1 user,  load average: 1.91, 1.37, 1.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -97,15 +97,8 @@ void *memmove(void *dst, const void *src, size_t n) {
   return ret;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  char *dst = (char *)out;
-  char *src = (char *)in;
-  while (n--) {
-    *dst = *src;
-    dst++;
-    src++;
-  }
-  return out;
+void *memcpy(void *out, const void *in, size_t n) { // 复制内存块
+  return memmove(out, in, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
