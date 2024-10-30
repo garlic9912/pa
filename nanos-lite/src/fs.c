d49e35bb94118da20ab253bcc51e6019ac72df9b
tracer-ics2023 <tracer@njuics.org>
2024-10-30 09:05:01 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:05:01 up 39 min,  1 user,  load average: 4.71, 3.98, 3.23

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -45,6 +45,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   // 获取文件 fd,也就是文件表的对应下标
   int fd = -1;
   for (int i = 0; i < sizeof(file_table)/sizeof(Finfo); i++) {
+    panic("%s", file_table[i].name);
     if (strcmp(pathname, file_table[i].name) == 0) {
       fd = i;
       return fd;
