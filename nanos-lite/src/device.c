b954578c517e9e5d04fc5e338053eda9c68f5956
tracer-ics2023 <tracer@njuics.org>
2024-10-30 14:42:41 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:42:41 up  6:16,  1 user,  load average: 1.02, 0.90, 0.84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -51,7 +51,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   // panic("%d", offset);
   x = (offset / 4) % 400;
   y = ((offset / 4) - x) / 400;
-  if(++flag == 128) panic("%d", y);
+  if(++flag == 1) panic("%d", y);
 
   w = len / 4;
   io_write(AM_GPU_FBDRAW, x, y, (int *)buf, w, 1, true);
