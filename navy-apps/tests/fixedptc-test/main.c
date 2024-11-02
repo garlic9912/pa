38b6f0e4457ed7df88c629de119cc846a321a041
tracer-ics2023 <tracer@njuics.org>
2024-11-02 16:06:23 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:06:23 up  6:54,  1 user,  load average: 1.14, 0.71, 0.55

--- navy-apps/tests/fixedptc-test/main.c
+++ navy-apps/tests/fixedptc-test/main.c
@@ -2,9 +2,10 @@
 #include </home/garlic/ics2023/navy-apps/libs/libfixedptc/include/fixedptc.h>
 
 int main() {
-    fixedpt A = fixedpt_rconst(1.8);
+    // fixedpt fixedpt_muli
+    fixedpt A = fixedpt_rconst(-1.8);
     fixedpt B = fixedpt_muli(A, 3);
-    // printf("%d\n", 3*1.8);
     printf("%d\n", fixedpt_toint(B));
+    // 
     return 0;
 }
