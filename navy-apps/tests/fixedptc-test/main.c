d11ccfae88162ac499fa63c2df3cbd4db6922211
tracer-ics2023 <tracer@njuics.org>
2024-11-02 16:14:53 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:14:53 up  7:03,  1 user,  load average: 1.00, 0.84, 0.67

--- navy-apps/tests/fixedptc-test/main.c
+++ navy-apps/tests/fixedptc-test/main.c
@@ -5,15 +5,27 @@ int main() {
     // fixedpt fixedpt_muli
     fixedpt A1 = fixedpt_rconst(-1.8);
     fixedpt B1 = fixedpt_muli(A1, 3);
-    printf("%d\n", fixedpt_toint(B1));
+    printf("%d\n", fixedpt_toint(B1));  // -6
     // fixedpt fixedpt_divi(fixedpt A, int B)
     fixedpt A2 = fixedpt_rconst(6.8);
     fixedpt B2 = fixedpt_divi(A2, 3);
-    printf("%d\n", fixedpt_toint(B2));
+    printf("%d\n", fixedpt_toint(B2));  // 2
     // fixedpt fixedpt_mul(fixedpt A, fixedpt B)
     fixedpt A3 = fixedpt_rconst(7.8);
     fixedpt B3 = fixedpt_rconst(1.1);
     fixedpt C3 = fixedpt_mul(A3, B3);
-    printf("%d\n", fixedpt_toint(C3));
+    printf("%d\n", fixedpt_toint(C3));  // 8   
+    // fixedpt fixedpt_div(fixedpt A, fixedpt B)
+    fixedpt A4 = fixedpt_rconst(7.8);
+    fixedpt B4 = fixedpt_rconst(1.1);
+    fixedpt C4 = fixedpt_div(A4, B4);
+    printf("%d\n", fixedpt_toint(C4));  // 7
+    // fixedpt fixedpt_abs(fixedpt A)
+    fixedpt A5 = fixedpt_rconst(7.8);
+    fixedpt B5 = fixedpt_rconst(-6.9);
+    fixedpt C5 = fixedpt_abs(A5);
+    fixedpt D5 = fixedpt_abs(B5);
+    printf("%d\n", fixedpt_toint(C5));  // 7    
+    printf("%d\n", fixedpt_toint(D5));  // -7   
     return 0;
 }
