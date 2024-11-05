e048b7d9ab772c507bf36f418c5deb18b3e9e0e5
tracer-ics2023 <tracer@njuics.org>
2024-11-05 17:22:07 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:22:07 up  5:36,  1 user,  load average: 0.82, 1.08, 1.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,6 +61,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 
+static int flag = 0;
 int fs_read(int fd, void *buf, size_t len) {
   // 处理按键事件
   switch (fd)
@@ -92,7 +93,7 @@ int fs_read(int fd, void *buf, size_t len) {
   file_table[fd].read(buf, start_offset, len);
   // 更新读写指针
   file_table[fd].open_offset = open_offset + len;
-  panic("1111111111111111");
+  if(++flag == 2) panic("1111111111111111");
   return len;
 }
 
