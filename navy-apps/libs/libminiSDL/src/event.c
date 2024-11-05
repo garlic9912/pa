e3ebc0924534c50a90f44138d577bd86f075a3af
tracer-ics2023 <tracer@njuics.org>
2024-11-05 16:15:45 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:15:45 up  4:29,  1 user,  load average: 1.59, 1.10, 0.82

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -22,12 +22,15 @@ int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
   // Read Error
   if (NDL_PollEvent(buf, sizeof(buf)) == 0) 
-    return;
+    return -1;
 
   char keyname[10], status[5];
   sscanf("%s %s", keyname, status);
-  if (strcmp(status, "DOWN")) event.type = SDL_KEYDOWN;
-  if (strcmp(keyname, "J"))
+  if (strcmp(status, "DOWN")) event->type = SDL_KEYDOWN;
+  if (strcmp(keyname, "J")) event->key.keysym.sym = SDLK_J;
+  if (strcmp(keyname, "UP")) event->key.keysym.sym = SDLK_UP;
+  if (strcmp(keyname, "K")) event->key.keysym.sym = SDLK_K;
+  if (strcmp(keyname, "DOWN")) event->key.keysym.sym = SDLK_DOWN;
   return 1;
 }
 
