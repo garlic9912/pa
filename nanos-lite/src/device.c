9a9a9da378fe3ed5fd89a91ad5f23725b839b2b5
tracer-ics2023 <tracer@njuics.org>
2024-11-06 21:52:17 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:52:17 up  6:14,  1 user,  load average: 0.91, 0.88, 0.53

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,8 +31,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   // 假设一次只读出一个事件
   char ev_info[64];
   int count = sprintf(ev_info, "%d %s", ev.keycode, ev.keydown ? "DOWN" : "UP");
-  printf("%d\n", count);
   memcpy((char *)buf, ev_info, count);
+  printf("%s\n", buf);
   return (ev.keycode << 1) + ev.keydown ? 1 : 0;
 }
 
