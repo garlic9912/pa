816932fe637fa7479f9eaab1dae28e13a6453d75
tracer-ics2023 <tracer@njuics.org>
2024-11-07 14:33:13 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:33:13 up  2:26,  1 user,  load average: 0.61, 0.88, 0.86

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -26,7 +26,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
-  // printf("11111111111111111111111111\n");
+  printf("%d\n", hdr.bitcount);
   if (hdr.bitcount != 24) return NULL;
   // printf("22222222222222222222222222\n");
   if (hdr.compression != 0) return NULL;
