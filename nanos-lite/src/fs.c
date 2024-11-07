5f263217c86a9c5f8cc582289fd266d3bcd9453c
tracer-ics2023 <tracer@njuics.org>
2024-11-07 15:16:27 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:16:27 up  3:10,  1 user,  load average: 0.82, 0.54, 0.48

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,6 +51,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   for (int i = 0; i < sizeof(file_table)/sizeof(Finfo); i++) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       fd = i;
+      file_table[fd].open_offset = 0;
       return fd;
     }
   }
