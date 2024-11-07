d0f2d304ccc318a230ea605f73457e80213eee5d
tracer-ics2023 <tracer@njuics.org>
2024-11-07 14:02:50 +0800

    >  compile NEMU 2246257278 夏文涛 Linux garlic-Inspiron-16-5620 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:50 up  1:56,  1 user,  load average: 0.90, 0.81, 0.65

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,5 @@
 #include <NDL.h>
 #include <SDL.h>
-#include <string.h>
 
 #define keyname(k) #k,
 
@@ -18,35 +17,18 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 
-static void get_first_word(const char *buf, char *first_word) {
-    int i = 0;
-
-    // 读取第一个单词，直到遇到空格或字符串结束
-    while (buf[i] != '\0' && !isspace(buf[i])) {
-        first_word[i] = buf[i];
-        i++;
-    }
-    first_word[i] = '\0';  // 终止字符串
-}
-
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
-  int keycode = NDL_PollEvent(buf, sizeof(buf));
-  if (keycode)
-  {
-    char *first_word[8];
-    get_first_word(buf, first_word);  // 获取按下还松开
-    int up_down = strcmp("ku", first_word); // 非0为按下，不为0为松开
-    if (up_down)  // 按下
-    {
-      event->type = SDL_KEYDOWN;
-    }
-    else
-    {
-      event->type = SDL_KEYUP;
-    }
-    event->key.keysym.sym = keycode;
+  int code = 0;
+  code = NDL_PollEvent(buf, sizeof(buf));
+  // DOWN or UP
+  if (code & 1) {
+    event->type = SDL_KEYDOWN;
+    // KeyCode
+    event->key.keysym.sym = (code-1) / 2;
+    // printf("%d\n", event->key.keysym.sym);
   }
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
